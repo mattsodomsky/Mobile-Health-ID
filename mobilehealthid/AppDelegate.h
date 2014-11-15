ab7114e57b7b7d913aceecb4b220f55521c51517
Matt <mattsodomsky@Matts-MacBook-Air.local>
2014-11-15 09:07:03 -0600

    Initial Commit

---
+++ mobilehealthid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobilehealthid
+//
+//  Created by Matt Sodomsky on 2014-11-15.
+//  Copyright (c) 2014 Matt Sodomsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobilehealthid/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mobilehealthid
+//
+//  Created by Matt Sodomsky on 2014-11-15.
+//  Copyright (c) 2014 Matt Sodomsky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
